282488d50985a454ea67b22de60f40c5a419d583
Mark Martinez <mark.martinez8230@gmail.com>
2022-10-10 18:15:23 -0500

    edited PMD file, incorporated some flask info

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
 #for flask stuff
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the Maternal Mortality Rate API.<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/   <br/>"
+    )
+
+
+
+
+
+
+
+if __name__ == "__main__":
+app.run(debug=True)
